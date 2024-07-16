3e1396f7fe9362ecb170f370abe66465d818b380
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2024-07-16 23:27:00 +0300

    Referansların verilmesi,Migration işlemleri ve Identity Kütüphanesinin eklenmesi,Asp.NetUser tablosunun özelleştirilmesi.

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;port=3306;database=EasyCashDB;user=root;password=12345678",
+                                        new MySqlServerVersion(new Version(8, 0, 28))); // MySQL server version
+            }
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProceses { get; set; }
+    }
+}
