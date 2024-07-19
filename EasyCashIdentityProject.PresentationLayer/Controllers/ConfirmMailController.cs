0978ad930939d7f490abc44cc337338f5805a0bb
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2024-07-19 11:42:01 +0300

    Register ve login işlemlerinin tamamlanması

--- EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -2,6 +2,7 @@
 using EasyCashIdentityProject.PresentationLayer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
@@ -26,12 +27,40 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
         {
-            var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
-            if (user.ConfirmCode==confirmMailViewModel.ConfirmCode)
+            var users = await _userManager.Users.Where(u => u.Email == confirmMailViewModel.Mail).ToListAsync();
+            if (users.Count > 1)
             {
-                return RedirectToAction("Index","MyProfile");
+                // Aynı e-posta adresine sahip birden fazla kullanıcı bulundu.
+                TempData["ErrorMessage"] = "Birden fazla kullanıcı aynı e-posta adresine sahip.";
+                return RedirectToAction("Index");
             }
-            return View();
+
+            var user = users.SingleOrDefault();
+            if (user == null)
+            {
+                // Kullanıcı bulunamadı.
+                TempData["ErrorMessage"] = "Kullanıcı bulunamadı.";
+                return RedirectToAction("Index");
+            }
+
+            if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            {
+                user.EmailConfirmed = true;
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "Login");
+            }
+
+            TempData["ErrorMessage"] = "Doğrulama kodu geçersiz.";
+            return RedirectToAction("Index");
         }
-    }
+
+    //    var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
+    //        if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+    //        {
+    //            user.EmailConfirmed = true;
+    //            await _userManager.UpdateAsync(user);
+    //            return RedirectToAction("Index", "Login");
+    //}
+    //        return View();
+}
 }
